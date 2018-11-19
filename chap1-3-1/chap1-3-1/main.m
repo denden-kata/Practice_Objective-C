471ea2010a4021650614f1fa4a9ee9ae0b0b6384
denden-kata <denden3104@gmail.com>
2018-11-19 19:01:24 +0900

    Initial Commit

---
+++ chap1-3-1/chap1-3-1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  chap1-3-1
+//
+//  Created by katatumuri on 2018/11/19.
+//  Copyright © 2018年 katatumuri. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
