857582e724dc6c4f94cf14d468a7fc5ecdb706a8
denden-kata <denden3104@gmail.com>
2018-11-21 12:55:47 +0900

    Update main.m
    test

--- chap1-3-1/chap1-3-1/main.m
+++ chap1-3-1/chap1-3-1/main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  chap1-3-1
-//
-//  Created by katatumuri on 2018/11/19.
-//  Copyright © 2018年 katatumuri. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
