8b408c92a0d4bdff87846e6b94f050e6db463ede
denden-kata <denden3104@gmail.com>
2018-11-24 18:57:12 +0900

    評価式の追加
    ちょっと注意が必要

--- chap1-3-1/chap1-3-1/main.m
+++ chap1-3-1/chap1-3-1/main.m
@@ -1,7 +1,21 @@
 #import <Foundation/Foundation.h>
 
+// 関数もCと同じ通りに書いて問題なし
+void Expression(bool compare){
+    // Bool型の注意 Objective-C ではYES or NOを使う
+    // Yes を %d に入れると 1 になる
+    NSLog(@"YESの値は \"%d\" です", compare);
+    
+    // 評価は yes = 1, no = 0
+    NSLog(@"論理式 1<3 の評価は \"%d\" です", 1<3);
+    
+    return;
+}
+
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {          // メモリ管理を行う、ひとまず書け
+    // @autoreleasepool ：「自動解放プールの生成」
+    // このブロック内で登録されたインスタンス（メモリ上のデータ）は終了時に解放される
+    @autoreleasepool {
         
         int num = 1;
         bool compare_bool = YES;
@@ -9,14 +23,10 @@ int main(int argc, const char * argv[]) {
         NSLog(@"Hello, World!");
         NSLog(@"num = %d", num);
         
-        // 評価は yes = 1, no = 0
-        NSLog(@"論理式 1<3 の評価は \"%d\" です", 1<3);
-        // Bool型の注意 Objective-C ではYES or NOを使う
-        // Yes を %d に入れると 1 になる
-        NSLog(@"YESの値は \"%d\" です", compare_bool);
-        
-        
+        // Expression : 評価式の意味
+        Expression(compare_bool);
         
     }
     return 0;
 }
+
