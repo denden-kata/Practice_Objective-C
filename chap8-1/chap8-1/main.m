34c8ab957924b97920bb50a141e65e84e784a73d
denden-kata <denden3104@gmail.com>
2018-11-26 16:01:29 +0900

    構造体の宣言、メンバ変数への格納を追加

--- chap8-1/chap8-1/main.m
+++ chap8-1/chap8-1/main.m
@@ -5,6 +5,40 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         NSLog(@"構造体やるよー\n");
+        
+        // "struct Person" が "構造体の型名"
+        // "struct" は必須
+        struct Person_sample{
+            float height;
+            float weight;
+            int birth_year;
+            int birth_month;
+            int birth_day;
+        };
+        
+        // "typedef"を用いて、型名を別の型名に変更
+        // "struct Person_sample" → "Person_sample" 短くなりました
+        typedef struct Person_sample Person_sample;
+        
+        
+        // 上記の２つの処理を１つにまとめたもの
+        typedef struct Person{
+            float height;
+            float weight;
+            int birth_year;
+            int birth_month;
+            int birth_day;
+        } Person;
+        
+        // 構造体Person型を使用する
+        Person human_a;
+        
+        // 構造体内のメンバ変数に値を格納する
+        human_a.height = 170;
+        
+        
+        
+        
     }
     return 0;
 }
