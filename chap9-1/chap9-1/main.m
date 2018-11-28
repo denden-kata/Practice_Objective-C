4326836a0bd09fa4f1eeb43358e13fb6882a7c92
denden-kata <denden3104@gmail.com>
2018-11-28 15:07:52 +0900

    クラスの書き方について記載

--- chap9-1/chap9-1/main.m
+++ chap9-1/chap9-1/main.m
@@ -1,17 +1,34 @@
-//
-//  main.m
-//  chap9-1
-//
-//  Created by katatumuri on 2018/11/28.
-//  Copyright © 2018年 katatumuri. All rights reserved.
-//
+// クラスについて
 
 #import <Foundation/Foundation.h>
 
+// Triangle : クラス
+// NSObject : スーパークラス
+@interface Triangle : NSObject {
+@public
+    // インスタンス変数
+    int width;
+    int height;
+}
+@end
+
+// あとで
+@implementation Triangle
+@end
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        // インスタンスを生成。宣言時は "*" をつける
+        Triangle *triangle_a = [[Triangle alloc] init];
+        
+        // インスタンス変数に値を格納。アロー演算子を用いる
+        triangle_a -> width = 6;
+        triangle_a -> height = 4;
+        
+        NSLog(@"triangle_a : width = %d, height = %d",
+              triangle_a -> width, triangle_a -> height);
+        
     }
     return 0;
 }
