e4fe1dc7356f4e805ff22b8efa4b59dd40e51ad0
denden-kata <denden3104@gmail.com>
2018-11-28 11:51:38 +0900

    クラス用のプロジェクトファイルを追加

---
+++ chap9-1/chap9-1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  chap9-1
+//
+//  Created by katatumuri on 2018/11/28.
+//  Copyright © 2018年 katatumuri. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
